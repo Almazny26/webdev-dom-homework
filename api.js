a09a4e489f9bee137a39e214224682694ad5806a
Dmitrij Karabanov <dmitrij.karabanov.ru>
2024-07-22 19:23:29 +0300

    перенёс post запрос в api.js

--- api.js
+++ api.js
@@ -9,3 +9,30 @@ export function getTodos(){
           return response.json();
         });
 }
+
+export function postTodo() {
+    return fetch("https://wedev-api.sky.pro/api/v1/dmitry-karabanov/comments", {
+        method: "POST",
+        body: JSON.stringify({
+          text: inputCommentEl.value,
+          name: inputNameEl.value,
+          forceError: false  // Добавляем параметр forceError для имитации ошибок 500
+        })
+      })
+        .then(response => {
+          // Обработка 400-й ошибки
+          if (response.status === 400) {
+            return response.json().then(error => {
+              throw new Error(`${error.error}`);
+            });
+          }
+          // Обработка 500-й ошибки
+          if (response.status === 500) {
+            throw new Error('Ошибка 500: Серверная ошибка.');
+          }
+          // Обработка успешного ответа
+          if (response.ok) {
+            return response.json();
+          }
+        });
+}
