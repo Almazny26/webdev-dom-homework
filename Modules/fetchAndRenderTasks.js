0c7d2a1fad6e6637b8da50819f2cf5d9e0dc12e0
Dmitrij Karabanov <dmitrij.karabanov.ru>
2025-08-18 23:35:07 +0300

    реализовал загрузчик при обновлении страницы

--- Modules/fetchAndRenderTasks.js
+++ Modules/fetchAndRenderTasks.js
@@ -2,6 +2,12 @@ import { renderUsers } from './render.js'
 import { updateUsers } from './users.js'
 
 export const fetchAndRenderTasks = () => {
+    // Показываем индикатор загрузки
+    const loadingEl = document.getElementById('loading')
+    if (loadingEl) {
+        loadingEl.classList.remove('hidden')
+    }
+
     // Загружаем комментарии с сервера
     return fetch('https://wedev-api.sky.pro/api/v1/dmitry-karabanov/comments')
         .then((response) => {
@@ -13,5 +19,17 @@ export const fetchAndRenderTasks = () => {
             updateUsers(data.comments)
             // Отрисовываем комментарии на странице
             renderUsers()
+
+            // Скрываем индикатор загрузки
+            if (loadingEl) {
+                loadingEl.classList.add('hidden')
+            }
+        })
+        .catch((error) => {
+            console.error('Ошибка при загрузке комментариев:', error)
+            // Скрываем индикатор загрузки даже при ошибке
+            if (loadingEl) {
+                loadingEl.classList.add('hidden')
+            }
         })
 }
